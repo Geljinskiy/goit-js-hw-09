5900b3a039331f73cb342e3003a9e5e2eabcb458
Geljinskiy <savvageljinskiy@gmail.com>
2022-09-21 23:53:42 +0300

    Now it must work

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,5 +1,9 @@
-import getRandomHexColor from './randomColor';
-('./randomColor');
+// import getRandomHexColor from './randomColor';
+// ('./randomColor');
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
 const fullSize = document.querySelector('.full-size');
 fullSize.style.height = window.innerHeight;
@@ -7,6 +11,8 @@ fullSize.style.height = window.innerHeight;
 const startBtn = document.querySelector('[data-start]');
 const stopBtn = document.querySelector('[data-stop]');
 const body = document.querySelector('body');
+
+let timerId = null;
 
 stopBtn.setAttribute('disabled', '');
 
@@ -18,7 +24,7 @@ function onStartBtnCLick() {
   if (!startBtn.hasAttribute('disabled')) {
     startBtn.setAttribute('disabled', '');
     stopBtn.removeAttribute('disabled');
-    timerColorChange = setInterval(() => {
+    timerId = setInterval(() => {
       body.style.backgroundColor = getRandomHexColor();
     }, 1000);
   }
@@ -27,5 +33,5 @@ function onStartBtnCLick() {
 function onStopBtnClick() {
   stopBtn.setAttribute('disabled', '');
   startBtn.removeAttribute('disabled');
-  clearInterval(timerColorChange);
+  clearInterval(timerId);
 }
