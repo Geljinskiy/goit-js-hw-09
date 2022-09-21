219881b9701aab33f09df4dc28e7b39d5615a7d6
Geljinskiy <savvageljinskiy@gmail.com>
2022-09-21 23:56:45 +0300

    I got it

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,5 +1,5 @@
-// import getRandomHexColor from './randomColor';
-// ('./randomColor');
+import getRandomHexColor from './randomColor';
+('./randomColor');
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
