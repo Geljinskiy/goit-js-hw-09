cfcb28de8cceab177fe207ae528d7eb70c2c2d90
Geljinskiy <savvageljinskiy@gmail.com>
2022-09-21 23:27:19 +0300

    will it work now?

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,13 +1,20 @@
 import getRandomHexColor from './randomColor';
 ('./randomColor');
 
+const fullSize = document.querySelector('.full-size');
+fullSize.style.height = window.innerHeight;
+
 const startBtn = document.querySelector('[data-start]');
 const stopBtn = document.querySelector('[data-stop]');
 const body = document.querySelector('body');
 
 stopBtn.setAttribute('disabled', '');
 
-startBtn.addEventListener('click', () => {
+startBtn.addEventListener('click', onStartBtnCLick);
+
+stopBtn.addEventListener('click', onStopBtnClick);
+
+function onStartBtnCLick() {
   if (!startBtn.hasAttribute('disabled')) {
     startBtn.setAttribute('disabled', '');
     stopBtn.removeAttribute('disabled');
@@ -15,13 +22,10 @@ startBtn.addEventListener('click', () => {
       body.style.backgroundColor = getRandomHexColor();
     }, 1000);
   }
-});
+}
 
-stopBtn.addEventListener('click', () => {
+function onStopBtnClick() {
   stopBtn.setAttribute('disabled', '');
   startBtn.removeAttribute('disabled');
   clearInterval(timerColorChange);
-});
-
-const fullSize = document.querySelector('.full-size');
-fullSize.style.height = window.innerHeight;
+}
